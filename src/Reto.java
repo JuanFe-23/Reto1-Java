19fd04b1d7d222943945c33690dded58544c3227
Juan Linares <juanzlinares29@gmail.com>
2024-11-26 16:39:28 -0500

    Modificacion menu

--- src/Reto.java
+++ src/Reto.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Reto {
     public static void main(String[] args) throws Exception {
 
+        System.out.println("===========================================");
+        System.out.println("Vamos a otro planeta");
+        System.out.println("----------------------------------------");
+
         Scanner sc = new Scanner(System.in);
 
         showMenu(sc);
@@ -12,60 +16,80 @@ public class Reto {
 
     private static void showMenu(Scanner sc) {
 
-        int option;
+        boolean continuee = true;
+        String destination = "Ninguno";
+        String transport = "No seleccionado";
 
         do {
+            System.out.println("\nMenú principal");
+            System.out.println("1. Selecionar destino (Planeta)");
+            System.out.println("2. Modificar viaje");
+            System.out.println("3. Confirmar y DESPEGAR");
+            System.out.println("4. Salir");
+            System.out.print("Elige un opción: ");
+            int option = sc.nextInt();
+            sc.nextInt();
 
-            System.out.println("===========================================");
-            System.out.println("Vamos a otro planeta");
-            System.out.println("----------------------------------------");
-            System.out.println("1. Mercurio");
-            System.out.println("2. Venus");
-            System.out.println("3. Marte");
-            System.out.println("4. Júpiter");
-            System.out.println("5. Saturno");
-            System.out.println("6. Urano");
-            System.out.println("7. Neptuno");
-            System.out.println("8. Salir");
-            System.out.print("Selecciona el planeta al que deseas viajar: ");
-            option = sc.nextInt();
-            sc.nextLine();
             switch (option) {
                 case 1:
-                    showPlanetData("Mercurio", 77.3, 50000);
-                    break;
-                case 2:
-                    showPlanetData("Venus", 41.4, 50000);
-                    break;
-                case 3:
-                    showPlanetData("Marte", 78.3, 50000);
-                    break;
-                case 4:
-                    showPlanetData("Júpiter", 628.7, 50000);
-                    break;
-                case 5:
-                    showPlanetData("Saturno", 1275.0, 50000);
-                    break;
-                case 6:
-                    showPlanetData("Urano", 2721.0, 50000);
-                    break;
-                case 7:
-                    showPlanetData("Neptuno", 4351.0, 50000);
-                    break;
-                case 8:
-                    System.out.println("Regresa cuando estes seguro de viajar a algun planeta");
-                    break;
+                    System.out.println("\n---Planetas disponobles---");
+                    System.out.println("1. Mercurio");
+                    System.out.println("2. Venus");
+                    System.out.println("3. Marte");
+                    System.out.println("4. Júpiter");
+                    System.out.println("5. Saturno");
+                    System.out.println("6. Urano");
+                    System.out.println("7. Neptuno");
+                    System.out.println("8. Salir");
+                    System.out.print("Selecciona el planeta al que deseas viajar: ");
+                    int planetOption = sc.nextInt();
+                    sc.nextLine();
 
-                default:
-                    System.err.println("Opción inválida");
-                    break;
+                    switch (planetOption) {
+                        case 1:
+                            showPlanetData("Mercurio", 77.3, 50000);
+                            break;
+                        case 2:
+                            showPlanetData("Venus", 41.4, 50000);
+                            break;
+                        case 3:
+                            showPlanetData("Marte", 78.3, 50000);
+                            break;
+                        case 4:
+                            showPlanetData("Júpiter", 628.7, 50000);
+                            break;
+                        case 5:
+                            showPlanetData("Saturno", 1275.0, 50000);
+                            break;
+                        case 6:
+                            showPlanetData("Urano", 2721.0, 50000);
+                            break;
+                        case 7:
+                            showPlanetData("Neptuno", 4351.0, 50000);
+                            break;
+                         case 8:
+                            System.out.println("Regresa cuando estes seguro de viajar a algun planeta");
+                            break;
+
+                        default:
+                            System.err.println("Opción inválida");
+                            break;
+                    }
+
             }
-            if (option != 8) {
-                pressEnter(sc);
+        } while (continuee) ;
 
-            }
-
-        } while (option != 8);
+        /*
+         * do {
+         * 
+         * 
+         * 
+         * 
+         * 
+         * }
+         * 
+         * } while (option != 8);
+         */
 
     }
 
